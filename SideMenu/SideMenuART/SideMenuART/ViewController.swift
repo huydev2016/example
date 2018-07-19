50cc41dbd0302ea6dc11306e98056460192b88cb
Huỳnh Quang Huy <huydev2016@gmail.com>
2018-07-19 20:42:04 +0700

    no message

---
+++ SideMenu/SideMenuART/SideMenuART/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuART
+//
+//  Created by dept3 on 7/18/18.
+//  Copyright © 2018 dept2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
